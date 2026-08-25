883eed696240473dbf500f886012fcab5b5c5918
jamie-ha-choi <jamie.ha.choi@gmail.com>
2026-08-25 16:28:02 +1200

    Minor comment editing

--- FinalImplementation/Firmware/t28-Project1/t28-Project1/main.c
+++ FinalImplementation/Firmware/t28-Project1/t28-Project1/main.c
@@ -5,18 +5,18 @@
  * Author : Lingwu Cai, Bowen, Ziqi Meng, Jaime
  */ 
 
-//define frequency CPU to 2MHZ,using UL(unsigned long number).
+//Define frequency CPU to 2MHZ,using UL(unsigned long number).
 #define F_CPU 2000000UL
-//give access to ATmega hardware register.
+
+
+//Give access to ATmega hardware register, enable delay, 
+//provide fixed width integer, include functions created in uart.h.
 #include <avr/io.h>
-//enable delay.
 #include <util/delay.h>
-//provide fixed width integer.
 #include<stdint.h>
-//include functions crated in uart.h.
 #include "uart.h"
 
-// pre-define RMSVoltage,PeakCurrent,Power
+// Pre-define RMSVoltage, PeakCurrent, Power
 #define RMSVoltage     14.5
 #define PeakCurrent    125
 #define Power          1.60
@@ -24,16 +24,14 @@
 
 int main(void)
 {
-	//8 bit loop counter i for below loop.
+	// 8 bit loop counter i for below loop.
+	// Make a 16 bit variable to store number.
 	uint8_t i;
-	//make a 16 bit variable to store number.
 	uint16_t value;
 	
-	//Character array storing the RMS voltage label.
+	//Character arrays storing the RMS voltage, peak current, power labels.
 	char rms_text[] = "RMS Voltage is: ";
-	//character array storing the Peak current label.
 	char current_text[] = "Peak Current is: ";
-	//character array storing the power label.
 	char power_text[] = "Power is: ";
     while (1) 
     {
